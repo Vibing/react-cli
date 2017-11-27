d54e285d26e040fdbfb32df5e47ecca1472154af
of2600 <903254852@qq.com>
2017-11-27 19:49:25 +0800

    更新路由

--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,18 @@
 export default [
   {
     exact: true,
-    path: 'home',
+    path: '/home',
     render: props => System.import(/* webpackChunkName: "home" */ './apps/home')
   },
   {
     exact: true,
-    path: 'page1',
+    path: '/page1',
     render: props =>
       System.import(/* webpackChunkName: "page1" */ './apps/page1')
   },
   {
     exact: true,
-    path: 'page2',
+    path: '/page2',
     render: props =>
       System.import(/* webpackChunkName: "page2" */ './apps/page2')
   }
